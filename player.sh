30d47f0146f035b6300284814168521ab7277f4b
Madeja <madeja@muzeumgpe-chorzow.pl>
2021-03-09 18:57:39 +0100

    9.03.2021

--- player.sh
+++ player.sh
@@ -7,34 +7,26 @@ fnVol(){
 }
 
 fnPlSim(){
-    ckeckSession=$(sudo screen -ls kanal$1)
-    exitstatus=$?
-    if [ $exitstatus = 0 ]; then
-        true
-    else
-        sudo screen -dmS kanal$1 omxplayer $4 -o alsa:hw:$2,0 /home/pi/player-pi/sound/$3.mp3
-    fi
+    screen -dmS kanal$1 omxplayer $4 -o alsa:hw:$2,0 /home/pi/player-pi/sound/$3.mp3
 }
 
 fnPlOnce(){
-    sudo omxplayer -o alsa:hw:$1,0 /home/pi/player-pi/sound/$2.mp3
+    omxplayer -o alsa:hw:$1,0 /home/pi/player-pi/sound/$2.mp3
 }
 
 fnOff(){
     sessionName="kanal$1"
-    sudo screen -ls "$sessionName" | (
+    screen -ls "$sessionName" | (
     IFS=$(printf '\t');
     sed "s/^$IFS//" |
     while read -r name stuff; do
-        sudo screen -S "$name" -X quit
+        screen -S "$name" -X quit
     done
     )
 }
 
 fnVol "1" "Device"
 fnVol "2" "Device_1"
-fnVol "3" "Device_2"
-fnVol "3" "Device_3"
 
 
 while true; do
@@ -46,15 +38,25 @@ while true; do
         while true; do
             hourNow=`date +%-H`
             hourEnd=$(cat value/hourEnd.txt)
-            fnPlSim "1" "Device_1" "1" "--loop"
-            fnPlSim "2" "Device_2" "2"
+
+                       
+            fnPlOnce "Device" "poczatek"
+            fnPlSim "1" "Device_1" "lekcja" 
+            fnPlSim "2" "Device" "dzwonek"
+            echo "przed liczeniem"
+            sleep 9m 10s
+            echo "po liczeniu"
+            sudo killall screen
+            fnPlOnce "Device" "poczatek"
+            echo "przed liczeniem 2"
             sleep 1m
-            fnPlOnce "Device" "3"
+            echo "po liczenieniu 2"
 
             if [[ $hourNow -eq $hourEnd ]]; then
                 
                 fnOff "1"
                 fnOff "2"
+                sudo killall screen
                 break
             fi
         done
